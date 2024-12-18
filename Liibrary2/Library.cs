8980568413a5cded4cbf4c06108e26aefa4db11a
user <user@WIN-S3FLAGBG853>
2024-12-18 20:07:26 +0300

    InsertBook InsertAuthor GetAuthorID

--- Liibrary2/Library.cs
+++ Liibrary2/Library.cs
@@ -22,9 +22,9 @@ namespace Liibrary2
             connection = new SqlConnection(connectionString);
             Console.WriteLine(connectionString);
         }
-        public static void Select(string tables, string fields, string condition = "", int padding = 20)
+        public static void Select(string tables, string fields, string condition = "", int padding = 26)
         {
-            Console.WriteLine("\n===========================2");
+            Console.WriteLine("\n===========================");
             string cmd = $"SELECT {fields} FROM {tables} ";
             if (condition.Length > 0) cmd += $" WHERE condition";
             cmd += ";";
@@ -39,7 +39,7 @@ namespace Liibrary2
                     Console.Write(reader.GetName(i).ToString().PadRight(padding - 1));
                 }
                 //Console.WriteLine();
-                Console.WriteLine("\n-------------------------1");
+                Console.WriteLine("\n-------------------------");
                 while (reader.Read())
                 {
                     for (int j = 0; j < reader.FieldCount; j++)
@@ -48,7 +48,7 @@ namespace Liibrary2
                     }
                     Console.WriteLine();
                 }
-                Console.WriteLine("===========================2\n");
+                Console.WriteLine("===========================\n");
 
             }
             reader.Close();
@@ -57,12 +57,12 @@ namespace Liibrary2
 
         public static void Insert(string table, string fields, string values)
         {
-            Console.WriteLine("\n===========================2");
+            Console.WriteLine("\n===========================Insert");
             string cmd = $"INSERT {table}({fields}) VALUES ({values});";
             SqlCommand command = new SqlCommand(cmd, connection);
             connection.Open();
             command.ExecuteNonQuery();
-            Console.WriteLine("===========================2\n");
+            Console.WriteLine("===========================Insert\n");
 
             connection.Close();
         }
@@ -70,13 +70,12 @@ namespace Liibrary2
 
         public static void InsertParam(string table, string fields, string values)//, string passvord
         {
-            Console.WriteLine("\n===========================2");
-            string cmd = $"INSERT @table(@fields) VALUES (@values);";
+            Console.WriteLine("\n===========================InsertParam");
+            string cmd = "INSERT @table(@fields) VALUES (@values);";
             //SqlCommand command = new SqlCommand(cmd, connection);
             //connection.Open();
             //command.ExecuteNonQuery();
-            Console.WriteLine("===========================2\n");
-
+            
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 //await connection.OpenAsync();
@@ -94,16 +93,66 @@ namespace Liibrary2
                 SqlParameter ageParam = new SqlParameter("@fields", fields);
                 // добавляем параметр к команде
                 command.Parameters.Add(ageParam);
-                 // создаем параметр 
+                // создаем параметр 
                 SqlParameter valuesParam = new SqlParameter("@values", values);
                 // добавляем параметр к команде
                 command.Parameters.Add(valuesParam);
 
                 //int number = 
-                    command.ExecuteNonQueryAsync();
+                command.ExecuteNonQueryAsync();
                 //Console.WriteLine($"Добавлено объектов: {number}");
             }
             connection.Close();
+            Console.WriteLine("===========================InsertParam\n");
+        }
+
+        public static void InsertAuthor(int id, string last_name, string first_name)//
+        {
+            Console.WriteLine("\n===========================InsertAuthor");
+            string cmd = "INSERT Authors(author_id, last_name, first_name) VALUES (@id, @last_name, @first_name);";
+            SqlCommand command = new SqlCommand(cmd, connection);
+
+            SqlParameter p_id = new SqlParameter("@id", SqlDbType.Int); p_id.Value = id;
+            SqlParameter p_last_name = new SqlParameter("@last_name", SqlDbType.NVarChar, 150); p_last_name.Value = last_name;
+            SqlParameter p_first_name = new SqlParameter("@first_name", SqlDbType.NVarChar, 150); p_first_name.Value = first_name;
+
+            command.Parameters.Add(p_id);
+            command.Parameters.Add(p_last_name);
+            command.Parameters.Add(p_first_name);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine("===========================InsertAuthor\n");
+        }
+        public static void InsertBook(int book_id, string book_title, int book_size, string publish_year, string author)//DateTime
+        {
+            Console.WriteLine("===========================InsertBook\n");
+            string cmd = "INSERT Books(book_id,book_title,book_size,publish_year,author) " +
+                "VALUES (@book_id,@book_title,@book_size,@publish_year,@author);";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@book_id", SqlDbType.Int).Value = book_id;
+            command.Parameters.Add("@book_title", SqlDbType.NVarChar, 150).Value = book_title;
+            command.Parameters.Add("@book_size", SqlDbType.Int).Value = book_size;
+            command.Parameters.Add("@publish_year", SqlDbType.Date).Value = publish_year;
+            command.Parameters.Add("@author", SqlDbType.Int).Value = GetAuthorID(author);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine("===========================InsertBook\n");
+
+        }
+        public static int GetAuthorID(string full_name)//
+        {
+            string cmd = "SELECT author_id FROM Authors WHERE last_name=@last_name AND first_name=@first_name;";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            command.Parameters.Add("@first_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').First();
+            command.Parameters.Add("@last_name", SqlDbType.NVarChar, 150).Value = full_name.Split(' ').Last();
+
+            connection.Open();
+            int id = Convert.ToInt32(command.ExecuteScalar());
+            connection.Close();
+            return id;
         }
     }
 }
--- Liibrary2/Program.cs
+++ Liibrary2/Program.cs
@@ -14,14 +14,24 @@ namespace Liibrary2
     {
         static void Main(string[] args)
         {
+            
             //Console.WriteLine(Library.connectionString);
-            Library.InsertParam("Authors", "author_id,last_name,first_name" ,"12, 'Smit1','Alex'");//
-            Library.Select("Authors", "author_id,last_name,first_name" );//,"10, 'Mazin','Alexsandr'"
+            //Library.InsertAuthor(7, "Gadge","Randy");//
+            //Library.InsertAuthor(5, "Stroustrup", "Bjarn");//
+            //Library.InsertAuthor(12, "Smit1","Alex");//
+            //Library.InsertParam("Authors", "author_id,last_name,first_name" ,"12, 'Smit1','Alex'");//
             //Library.Insert("Books", "book_id,book_title,book_size,publish_year,author", "6,'Vicings',950,'10.10.2010',10");
-            Library.Select("Books", "book_id,book_title,book_size,publish_year,author");
+            Library.InsertBook(7, "C++ Programming Language", 331, "29.01.1986", "Bjarn Stroustrup");
             //Library.Select("author_id,first_name,last_name", "Authors");
             //Library.Select("book_title,publish_date,[Author] = first_name+' '+last_name",
-                 //"Books,Authors", "author=author_id",32);
+            //"Books,Authors", "author=author_id",32);
+            //Console.WriteLine(Library.GetAuthorID("Bjarn Stroustrup")); 
+            //Console.WriteLine(Library.GetAuthorID("Alexsandr Mazin"));
+
+
+
+            Library.Select("Authors", "author_id,last_name,first_name" );//,"10, 'Mazin','Alexsandr'"
+            Library.Select("Books", "book_id,book_title,book_size,publish_year,author");            
         }
     }
 }
