acf61c9e596ce6c827d8c0ce01cb4558814d9982
user <user@WIN-S3FLAGBG853>
2024-12-18 17:30:23 +0300

    Library.Insert

---
+++ Liibrary2/Library.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Liibrary2
+{
+
+    internal class Library
+    {
+        public static string connectionString = "";
+        static SqlConnection connection = null;
+        static Library()//public
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            Console.WriteLine(connectionString);
+        }
+        public static void Select(string tables,string fields,  string condition = "", int padding = 20)
+        {
+            Console.WriteLine("\n===========================2");
+            string cmd = $"SELECT {fields} FROM {tables} ";
+            if (condition.Length > 0) cmd += $" WHERE condition";
+            cmd += ";";
+            Console.WriteLine(cmd);
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader.GetName(i).ToString().PadRight(padding - 1));
+                }
+                //Console.WriteLine();
+                Console.WriteLine("\n-------------------------1");
+                while (reader.Read())
+                {
+                    for (int j = 0; j < reader.FieldCount; j++)
+                    {
+                        Console.Write(reader[j].ToString().PadRight(padding).Remove(padding - 1)); //   // + "\t\t"                   
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("===========================2\n");
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+
+        public static void Insert(string table, string fields, string values)
+        {
+            Console.WriteLine("\n===========================2");
+            string cmd = $"INSERT {table}({fields}) VALUES ({values});";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            Console.WriteLine("===========================2\n");
+
+            connection.Close();
+        }
+    }
+}
---
+++ Liibrary2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Liibrary2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Library.connectionString);
+            //Library.Insert("Authors", "author_id,last_name,first_name" ,"10, 'Mazin','Alexsandr'");//
+            Library.Select("Authors", "author_id,last_name,first_name" );//,"10, 'Mazin','Alexsandr'"
+            //Library.Insert("Books", "book_id,book_title,book_size,publish_year,author", "6,'Vicings',950,'10.10.2010',10");
+            Library.Select("Books", "book_id,book_title,book_size,publish_year,author");
+            //Library.Select("author_id,first_name,last_name", "Authors");
+            //Library.Select("book_title,publish_date,[Author] = first_name+' '+last_name",
+                 //"Books,Authors", "author=author_id",32);
+        }
+    }
+}
